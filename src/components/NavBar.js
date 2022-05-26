4480d756978d24407962726acffd490fda471e30
marisasaunders <marisaksaunders@gmail.com>
2022-05-26 16:48:35 -0400

    switching li to a tag

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,20 +4,20 @@ import { NavLink } from "react-router-dom";
 
 function NavBar() {
 
-  const link = {
-    width: '100px',
-    paddinng: '12px',
-    margin: '0 6px 6px',
-    backgroundColor: 'blue',
-    textDecoration: 'none',
-    color: 'orange',
-  }
+  // const link = {
+  //   width: '100px',
+  //   paddinng: '12px',
+  //   margin: '0 6px 6px',
+  //   backgroundColor: 'blue',
+  //   textDecoration: 'none',
+  //   color: 'orange',
+  // }
   
   return (
   <div className="navbar">
-    <li><NavLink to="/" style={link} exact activeStyle={{background: 'darkblue'}}>Home</NavLink></li>
-    <li><NavLink to="/add" style={link} exact activeStyle={{background: 'darkblue'}}>Add Music</NavLink></li>
-    <li><NavLink to="/about" style={link} exact activeStyle={{background: 'darkblue'}}>About</NavLink></li>
+    <a><NavLink to="/" exact activeStyle={{background: 'darkblue'}}>Home</NavLink></a>
+    <a><NavLink to="/add" exact activeStyle={{background: 'darkblue'}}>Add Music</NavLink></a>
+    <a><NavLink to="/about" exact activeStyle={{background: 'darkblue'}}>About</NavLink></a>
   </div>
   )
 }

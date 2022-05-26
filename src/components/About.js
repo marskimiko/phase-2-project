889a383546de370f334f09a459bc8bdb3ba90e00
marisasaunders <marisaksaunders@gmail.com>
2022-05-26 16:44:04 -0400

    about page styling

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 function About() {
   return (
-    <div>Marisa is a front end developer, she attended Flatiorn School in 2022, here is a link to her work https://github.com/marskimiko </div>
+    <div class="column-2">
+            <h1>About me</h1>
+            <p>Hi! I'm Sammy the Shark, Senior Selachimorpha at DigitalOcean by day, dabbler in all things dev by night. This site is a demonstration website for the tutorial series "<a href="https://www.digitalocean.com/community/tutorial_series/how-to-build-a-website-with-css">How To Build a Website With CSS</a>," which walks you through building and customizing this website from start to finish.</p>
+            <p>If you're following this tutorial series, you can replace this text with your own "About Me" content.</p>    
+        </div>
   )
 }
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -22,7 +22,7 @@ function Form({ onAddMusic, musics }) {
       width: '210px',
     },
     form: { 
-      backgroundColor: 'pink',
+      backgroundColor: '',
       maxWidth: "420px",
       margin: "50px auto",
       padding: "0px",
@@ -58,31 +58,51 @@ function Form({ onAddMusic, musics }) {
   }
 
   useEffect(() => {
-    if(params.id) {
+    if(params.id && musics.length > 0) {
       const musicWeWantToEdit = musics.find(music => music.id === parseInt(params.id))
       setFormData(musicWeWantToEdit);
+    } else {
+      setFormData(defaultFormData);
     }
 
-  }, [params])
+  }, [params, musics])
 
   return (
     <div>
-        <h2>Add a New Song</h2>
-       <form onSubmit={handleSubmit} style={styles.form}>
-       <div>
+        <h2 style={styles.form}>Add a New Song</h2>
+        <form onSubmit={handleSubmit} style={styles.form}>
+          <div>
             <label>Artist:</label>
-            <input type="text" name="artist" value={formData.artist} onChange={handleChange} />
-           </div>
-           <div>
-            <label>Name:</label>
-            <input type="text" name="name" value={formData.name} onChange={handleChange} />
-           </div>
-           <div>
+            <input 
+              type="text" 
+              name="artist" 
+              value={formData.artist} 
+              onChange={handleChange} 
+            />
+          </div>
+          <div>
+            <label>Album:</label>
+              <input 
+                type="text" 
+                name="name" 
+                value={formData.name} 
+                onChange={handleChange} 
+              />
+          </div>
+          <div>
             <label>Image:</label>
-            <input type="text" name="image" value={formData.image} onChange={handleChange} />
-           </div>
-            <input type="submit" value={formData.id ? "Update" : "Create"}/>
-       </form>
+              <input 
+                type="text" 
+                name="image" 
+                value={formData.image} 
+                onChange={handleChange} 
+              />
+          </div>
+            <input 
+              type="submit" 
+              value={formData.id ? "Update" : "Create"}
+            />
+        </form>
     </div>
 )
 }

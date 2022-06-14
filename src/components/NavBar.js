bad4d3a3003bb9ca469623220bf5fb7e77441488
marisasaunders <marisaksaunders@gmail.com>
2022-06-14 16:07:44 -0400

    delete a tags from NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,9 +6,9 @@ function NavBar() {
   
   return (
   <div className="topnav">
-    <a><NavLink to="/" exact activeStyle={{background: 'yellowgreen'}}>Home</NavLink></a>
-    <a><NavLink to="/add" exact activeStyle={{background: 'yellowgreen'}}>Add Music</NavLink></a>
-    <a><NavLink to="/about" exact activeStyle={{background: 'yellowgreen'}}>About</NavLink></a>
+    <NavLink to="/" exact activeStyle={{background: 'yellowgreen'}}>Home</NavLink>
+    <NavLink to="/add" exact activeStyle={{background: 'yellowgreen'}}>Add Music</NavLink>
+    <NavLink to="/about" exact activeStyle={{background: 'yellowgreen'}}>About</NavLink>
   </div>
   )
 }
